7b00254ffc0370633272ae30265e9f006dca19fb
Ömer Çağrı Bozdemir <92989192+cagribozdemir@users.noreply.github.com>
2022-09-29 21:33:25 +0300

    Add files via upload

---
+++ FeeCollector.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.8.7;
+// SPDX-License-Identifier: MIT
+
+contract FeeCollector {
+    address public owner;
+    uint256 public balance;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    receive() payable external {
+        balance += msg.value;
+    }
+
+    function withdraw(uint amount, address payable destAddr) public {
+        require(msg.sender == owner, "Only owner can withdraw");
+        require(amount <= balance, "Insufficient funds");
+        destAddr.transfer(amount);
+        balance -= amount;
+    }
+}
